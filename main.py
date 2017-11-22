949496fd3067a18aea2c5f0e92f558fe49a8c914
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-21 18:43:02 -0600

    wrote basic python

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import sys
 import random
+
+answer = True
+
+while answer:
+    question = raw_input("Ask a question: ")
+
+    answers = random.randint(1,5)
+
+    if question == "":
+        sys.exit()
+    elif answers == 1:
+        print "yeah"
+    elif answers == 2:
+        print "nope"
+    elif answers == 3:
+        print "maybe"
+    elif answers == 4:
+        print "i doubt it"
+    elif answers == 5:
+        print "hell nah"
