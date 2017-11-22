360ca3bd1ad253911b5fd65e09456d8f165fe25d
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-21 21:50:00 -0600

    program runs

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import random
 answer = True
 
 while answer:
-    question = raw_input("Ask a question: ")
+    question = raw_input("Ask a yes or no question. Press 'enter' to exit: ")
 
     answers = random.randint(1,5)
 
     if question == "":
+        print "exiting program..."
         sys.exit()
     elif answers == 1:
-        print "yeah"
+        print "Yes!"
     elif answers == 2:
-        print "nope"
+        print "Nope!"
     elif answers == 3:
-        print "maybe"
+        print "Maybe"
     elif answers == 4:
-        print "i doubt it"
+        print "I doubt it."
     elif answers == 5:
-        print "hell nah"
+        print "Hell no!"
